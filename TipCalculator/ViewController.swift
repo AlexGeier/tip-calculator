c8c57f72450f915beeff88b45e092d598a3a57a5
Alex Geier <alex_geier@icloud.com>
2020-01-13 19:09:48 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Alex Geier on 1/13/20.
+//  Copyright © 2020 Alex Geier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
